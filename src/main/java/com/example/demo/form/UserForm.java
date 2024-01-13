b30d666c5f53393cdc4243bce5dc1562e179e22a
TakanoriShima <quark2galaxy@gmail.com>
2024-01-13 15:47:20 +0900

    「Chapter 20 Spring Bootその2」done

---
+++ src/main/java/com/example/demo/form/UserForm.java
@@ -0,0 +1,20 @@
+package com.example.demo.form;
+
+// import追加
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class UserForm {
+
+	// nameへのバリデーション設定を追加
+	@Size(min = 1, max = 200)
+	private String name;
+
+	// emailへのバリデーション設定を追加
+	@Size(min = 1, max = 200)
+	@Email
+	private String email;
+}
